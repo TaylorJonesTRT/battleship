7af0ba2eaa6554d8b0dd66315be430f0cbb7763b
Taylor Jones <taylor@taylorwjones.com>
2021-02-21 17:14:42 -0600

    Changed gameboard from factory function to a class

--- src/logic/tests/gameboard.test.ts
+++ src/logic/tests/gameboard.test.ts
@@ -1,6 +1,6 @@
-import gameboard from '../code/gameboard';
+import Gameboard from '../code/gameboard';
 
 test('Gameboard: Making sure the createBoard function creates an empty board of 100 spots', () => {
-  const gameboardTest = gameboard();
-  expect(gameboardTest.createBoard(100)).toStrictEqual(new Array(100).fill(''));
+  const gameboardTest = new Gameboard();
+  expect(gameboardTest.board).toStrictEqual(new Array(100).fill({ hasShip: false, isShot: false }));
 });
