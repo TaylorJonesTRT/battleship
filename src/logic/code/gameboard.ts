f1e99d5dc14616e10c25ea19fa37848e927d1836
Taylor Jones <taylor@taylorwjones.com>
2021-02-25 20:36:53 -0600

    Added test to check if placeShip throws an error when a ship is placed on a single invalid spot and its logic. Now need to refactor to address all illegal placings

--- src/logic/code/gameboard.ts
+++ src/logic/code/gameboard.ts
@@ -9,24 +9,32 @@ class Gameboard {
   init = () => {
     for (let i = 0; i < 100; i++) {
       this.board.push({
-        hasShip:  false,
+        hasShip: false,
         isShot: false
       });
     }
   };
 
   placeShip = (start: number, shipLength: number, rotation: string) => {
-    if (rotation === 'hor') {
-      for (let i = 0; i < shipLength; i++) {
-        this.board[(start - 1) + i] = { hasShip: true, isShot: false };
+    // eslint-disable-next-line no-useless-catch
+    try {
+      if (start === 7 && shipLength > 4) throw new Error('Ship cannot be placed there');
+
+      if (rotation === 'hor') {
+        for (let i = 0; i < shipLength; i++) {
+          this.board[(start - 1) + i] = { hasShip: true, isShot: false };
+        }
+      }
+      if (rotation === 'vert') {
+        for (let i = 0; i < shipLength; i++) {
+          this.board[(start - 1) + 10] = { hasShip: true, isShot: false };
+        }
       }
     }
-    if (rotation === 'ver') {
-      for (let i = 0; i < shipLength; i++) {
-        this.board[(start - 1) + i] = { hasShip: true, isShot: false };
-      } 
+    catch (e) {
+      throw e;
+      console.log(e);
     }
-    return this.board;
   };
 }
 
--- src/logic/tests/gameboard.test.ts
+++ src/logic/tests/gameboard.test.ts
@@ -11,7 +11,25 @@ test('Gameboard: board (array) of 100 is initialized', () => {
 test('Gameboard: Placing a Ship sideways starting on spot 2', () => {
   const gameboardTest = new Gameboard();
   gameboardTest.placeShip(1, 4, 'hor');
-  expect(gameboardTest.board[1]).toStrictEqual({
+  expect(gameboardTest.board[0]).toStrictEqual({
     hasShip: true, isShot: false
   });
 });
+
+test('Gameboard: Placing a Ship vertically starting on spot 4', () => {
+  const gameboardTest = new Gameboard();
+  gameboardTest.placeShip(5, 4, 'vert');
+  expect(gameboardTest.board[14]).toStrictEqual({
+    hasShip: true, isShot: false
+  });
+});
+
+test('Placing a ship at a starting point that will make the ship go off the board should return an error', () => {
+  const gameboardTest = new Gameboard();
+  expect(() => {
+    gameboardTest.placeShip(7, 5, 'hor');
+  }).toThrow('Ship cannot be placed there');
+});
+
+// Todo: Need to add tests to make sure an error is returned if ship placed
+// todo: at a starting point where its end point will be "off the board"
